1927146bb68e06fef4d4387e0529e60ee5669e74
Alexys LAURENT <103429337+Alexys-LAURENT@users.noreply.github.com>
2024-04-16 16:12:57 +0200

    fix delete created row from commandes when error insert commandes_pieces|velos

--- utils/commandes/createCommande.ts
+++ utils/commandes/createCommande.ts
@@ -1,75 +1,78 @@
 'use server';
 
-import { createClient } from "../supabase/server";
+import { createClient } from '../supabase/server';
 import { getUserConnected } from '../getUserConnected';
 
-export async function createCommande(infos: { items: { id: string; nom: string; quantite: number; prix: number; image: string; type: "vélo" | "pièce" }[]; nom: string; prenom: string; adresse: string; ville: string; codePostal: string }) {
-    'use server';
+export async function createCommande(infos: {
+  items: { id: string; nom: string; quantite: number; prix: number; image: string; type: 'vélo' | 'pièce' }[];
+  nom: string;
+  prenom: string;
+  adresse: string;
+  ville: string;
+  codePostal: string;
+}) {
+  'use server';
 
-    const supabase = createClient();
+  const supabase = createClient();
 
-    const user = await getUserConnected();
+  const user = await getUserConnected();
 
+  const { data: dataCommande, error: errorCommande } = await supabase
+    .from('commandes')
+    .insert([
+      {
+        nom: infos.nom,
+        prenom: infos.prenom,
+        adresse: infos.adresse,
+        ville: infos.ville,
+        cp: infos.codePostal,
+        id_boutique: null,
+        id_vendeur: null,
+        id_client: user?.id,
+        status: 'En attente de traitement',
+      },
+    ])
+    .select()
+    .single();
 
-    const { data: dataCommande, error: errorCommande } = await supabase
-        .from('commandes')
-        .insert([
-            {
-                nom: infos.nom,
-                prenom: infos.prenom,
-                adresse: infos.adresse,
-                ville: infos.ville,
-                cp: infos.codePostal,
-                id_boutique: null,
-                id_vendeur: null,
-                id_client: user?.id,
-                status: 'En attente de traitement'
-            },
-        ])
-        .select()
-        .single();
+  if (errorCommande) {
+    console.log(errorCommande);
+    return false;
+  }
 
-    if (errorCommande) {
-        console.log(errorCommande);
-        return false;
+  // if atleast one product is a piece
+  if (infos.items.some((item) => item.type === 'pièce')) {
+    const { data: dataCommandePiece, error: errorCommandePiece } = await supabase.from('commandes_pieces').insert(
+      infos.items.map((item) => ({
+        id_piece: item.id,
+        id_commande: dataCommande.id_commande,
+        quantite: item.quantite,
+      }))
+    );
+
+    if (errorCommandePiece) {
+      await supabase.from('commandes').delete().eq('id_commande', dataCommande.id_commande);
+      console.log(errorCommandePiece);
+      return false;
     }
+  }
 
-    // if atleast one product is a piece
-    if (infos.items.some((item) => item.type === 'pièce')) {
-        const { data: dataCommandePiece, error: errorCommandePiece } = await supabase
-            .from('commandes_pieces')
-            .insert(
-                infos.items.map((item) => ({
-                    id_piece: item.id,
-                    id_commande: dataCommande.id_commande,
-                    quantite: item.quantite,
-                }))
-            );
+  // if atleast one product is a vélo
+  if (infos.items.some((item) => item.type === 'vélo')) {
+    const { data: dataCommandeVelo, error: errorCommandeVelo } = await supabase.from('commandes_velos').insert(
+      infos.items.map((item) => ({
+        id_velo: item.id,
+        id_commande: dataCommande.id_commande,
+        quantite: item.quantite,
+      }))
+    );
 
-        if (errorCommandePiece) {
-            console.log(errorCommandePiece);
-            return false;
-        }
+    if (errorCommandeVelo) {
+      await supabase.from('commandes').delete().eq('id_commande', dataCommande.id_commande);
+      console.log(errorCommandeVelo);
+      return false;
     }
+  }
 
-    // if atleast one product is a vélo
-    if (infos.items.some((item) => item.type === 'vélo')) {
-        const { data: dataCommandeVelo, error: errorCommandeVelo } = await supabase
-            .from('commandes_velos')
-            .insert(
-                infos.items.map((item) => ({
-                    id_velo: item.id,
-                    id_commande: dataCommande.id_commande,
-                    quantite: item.quantite,
-                }))
-            );
-
-        if (errorCommandeVelo) {
-            console.log(errorCommandeVelo);
-            return false;
-        }
-    }
-
-
-    return true;
+  return true;
 }
